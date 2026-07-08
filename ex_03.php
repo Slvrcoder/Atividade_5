8779b51ec33e747f76329537b15a9124a9a92c87
Slvrcoder <matheus_silveira9@sesisenai.org.br>
2026-07-08 08:28:31 -0300

    ex 3

--- ex_03.php
+++ ex_03.php
@@ -1,3 +1,14 @@
 <?php
 
-function mascararCPF
+function mascararCpf($cpf){
+
+$ultimos = substr($cpf, -4);
+
+$mascara = str_repeat("*", strlen($cpf) - 4);
+
+return $mascara . $ultimos;
+}
+
+$cpf = "08640111995";
+
+echo mascararCpf($cpf);
